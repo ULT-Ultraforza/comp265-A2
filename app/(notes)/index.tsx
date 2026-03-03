68a6d6b2fbeadab4325781738aac50a444c87efd
ULT-Ultraforza <thomascalab@outlook.com>
2026-03-02 20:34:43 -0600

    added archive/details/new.tsx as new screens for app

--- app/(notes)/index.tsx
+++ app/(notes)/index.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export default function NotesHome() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home / Notes Screen</Text>
-      <Link href={{ pathname: '/(notes)/details', params: { id: '123', title: 'My First Note' } }}>
+      <Link href={{ pathname: '/(notes)/details', params: { id: '123', title: 'My First Note' } }} asChild>
         <Text style={{ color: 'blue', marginTop: 20 }}>Go to Details</Text>
+      </Link>
+      <Link href="/(notes)/archive" asChild style={{marginTop: 20}}>
+        <Button title="Go to Archive" />
       </Link>
     </View>
   );
---
+++ app/(notes)/new.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function NotesHome() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home / Notes Screen</Text>
+            <Link href={{ pathname: '/(notes)/details', params: { id: '123', title: 'My First Note' } }}>
+                <Text style={{ fontSize: 24, textAlign: 'center', margin: 20 }}>
+                    New Note Screen
+                </Text>
+            </Link>
+        </View>
+    );
+}
